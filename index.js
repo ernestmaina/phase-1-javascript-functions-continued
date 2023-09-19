77d813a6b652db25a46c2eb54778d8b5fe2b049d
ErnestMaina <you@example.com>
2023-09-20 00:40:09 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+let defaultActivity = "roller-skate";
+
+
+function setDefaultActivity(activity) {
+  defaultActivity = activity;
+}
+
+function saturdayFun(activity) {
+  const usedActivity = activity || defaultActivity;
+  return `This Saturday, I want to ${usedActivity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  
+
+
+
+
+
+
+
+
+
+
